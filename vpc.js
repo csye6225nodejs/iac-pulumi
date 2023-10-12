90e0573aeae5712bf1f1e46466b3defee9761401
abhishekvenkata1997 <abhishekvenkata.199715@gmail.com>
2023-10-12 16:14:44 -0400

    Add changes for dynamic ip

--- vpc.js
+++ vpc.js
@@ -1,11 +1,14 @@
 const aws = require("@pulumi/aws");
 const pulumi = require("@pulumi/pulumi");
 const vpcCidrBlock = new pulumi.Config("iac-pulumi").require("vpcCidrBlock");
-const publicSubnetCidrs = new pulumi.Config("iac-pulumi").require("publicSubnetCidrs"); 
-const parsedPublicSubnetCidrs = JSON.parse(publicSubnetCidrs);
+//const publicSubnetCidrs = new pulumi.Config("iac-pulumi").require("publicSubnetCidrs"); 
+//const parsedPublicSubnetCidrs = JSON.parse(publicSubnetCidrs);
 
-const privateSubnetCidrs = new pulumi.Config("iac-pulumi").require("privateSubnetCidrs"); 
-const parsedPrivateSubnetCidrs = JSON.parse(privateSubnetCidrs);
+//const privateSubnetCidrs = new pulumi.Config("iac-pulumi").require("privateSubnetCidrs"); 
+//const parsedPrivateSubnetCidrs = JSON.parse(privateSubnetCidrs);
+
+const SubnetCIDRAdviser = require("subnet-cidr-calculator");
+const  subnetcidr = new pulumi.Config("iac-pulumi").require("subnetCidr");
 
 function createVPC() {
     const vpc = new aws.ec2.Vpc("my-vpc", {
@@ -25,12 +28,16 @@ async function createSubnets(vpc) {
     const publicSubnets = [];
     const privateSubnets = [];
 
+    const [ipAddress, subnetMask] = subnetcidr.split('/');
+    const probabal_subnets = SubnetCIDRAdviser.calculate(ipAddress, subnetMask);
+
     console.log(availabilityZones);
     availabilityZones.forEach((az, index) => {
 
+        
         const publicSubnet = new aws.ec2.Subnet(`public-subnet-${index}`, {
             vpcId: vpc.id,
-            cidrBlock: parsedPublicSubnetCidrs[index],
+            cidrBlock: probabal_subnets.subnets[index].value,
             availabilityZone: az,
             mapPublicIpOnLaunch: true,
             tags: { Name: `public-subnet-${index}` },
@@ -39,7 +46,7 @@ async function createSubnets(vpc) {
 
         const privateSubnet = new aws.ec2.Subnet(`private-subnet-${index}`, {
             vpcId: vpc.id,
-            cidrBlock: parsedPrivateSubnetCidrs[index],
+            cidrBlock: probabal_subnets.subnets[index+3].value,
             availabilityZone: az,
             tags: { Name: `private-subnet-${index}` },
         });
