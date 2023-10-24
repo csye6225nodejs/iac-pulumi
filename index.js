3157e30dbd4e644a624a74ed31250094a7bd7e0b
abhishekvenkata1997 <abhishekvenkata.199715@gmail.com>
2023-10-24 16:23:31 -0400

    add changes

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const ami_id = new pulumi.Config("iac-pulumi").require("ami_id");
 const { createVPC, createSubnets } = require("./vpc");
 const { createRdsInstance } = require('./rds');
 const { createInternetGateway, createPublicRouteTable, createPrivateRouteTable } = require("./networking");
+const { Endpoint } = require("@pulumi/aws/dms");
+const { RdsDbInstance } = require("@pulumi/aws/opsworks");
 const  subnetcidr = new pulumi.Config("iac-pulumi").require("subnetCidr");
 const  destinationCidr = new pulumi.Config("iac-pulumi").require("destinationCidr");
 
@@ -33,23 +35,6 @@ async function main() {
         name: "application-security-group",
         description: "Security group for web applications",
         vpcId: vpc.id, // Replace with your VPC ID
-    });
-
-    //create ec2 instance
-    const ec2Instance = new aws.ec2.Instance("my-ec2-instance", {
-        ami: ami_id, // Replace with your custom AMI ID
-        instanceType: "t2.micro",   
-        subnetId: publicSubnets[0],
-        vpcSecurityGroupIds: [applicationSecurityGroup.id], // Attach the security group
-        keyName: keyPair.keyName,
-        disableApiTermination: false, // No protection against accidental termination
-        rootBlockDevice: {
-            volumeSize: volumeSize, // Root volume size of 25 GB
-            volumeType: volumeType, // General Purpose SSD (GP2)
-        },
-        tags: {
-            Name: "Abhishek-EC2Instance", // Replace with a suitable name
-        },
     });
 
     const ingressPorts = JSON.parse(ports);
@@ -81,7 +66,46 @@ async function main() {
     const probabal_subnets = SubnetCIDRAdviser.calculate(ipAddress, 16);
 
     const dbSecurityGroup = await createRDSSecurityGroup(applicationSecurityGroup, vpc);
-    const rdsAddress = await createRdsInstance(vpc,privateSubnets );
+    const rdsInstance = await createRdsInstance(dbSecurityGroup,privateSubnets );
+
+    pulumi.all({
+        dbId: rdsInstance.dbInstanceIdentifier,
+        dbAddress: rdsInstance.address,
+        dbPort:    rdsInstance.port,
+    }).apply(async(outputs) => {
+        // Define EC2 userData script
+        const userDataScript = `#!/bin/bash
+            echo "export DB_NAME=${outputs.dbId}" >> /opt/webapp/.env
+            echo "export DB_PORT=${outputs.dbPort}" >> /opt/webapp/.env
+            echo "export DB_HOST=${outputs.dbAddress}" >> /opt/webapp/.env
+            source /etc/environment
+            # Start your Nodejs application here
+        `;
+    
+        // Create EC2 instance
+        const ec2Instance = new aws.ec2.Instance("my-ec2-instance", {
+            ami: ami_id, // Replace with your custom AMI ID
+            instanceType: "t2.micro",   
+            subnetId: publicSubnets[0],
+            vpcSecurityGroupIds: [applicationSecurityGroup.id], // Attach the security group
+            keyName: keyPair.keyName,
+            userData: userDataScript,
+            disableApiTermination: false, // No protection against accidental termination
+            rootBlockDevice: {
+                volumeSize: volumeSize, // Root volume size of 25 GB
+                volumeType: volumeType, // General Purpose SSD (GP2)
+            },
+            tags: {
+                Name: "Abhishek-EC2Instance", // Replace with a suitable name
+            },
+        }); 
+    
+    });
+    
+ 
+        //create ec2 instance
+    
+
     const publicRouteTable = createPublicRouteTable(vpc, publicSubnets, internetGateway);
     const privateRouteTable = createPrivateRouteTable(vpc, privateSubnets);
 }
@@ -95,9 +119,6 @@ async function createRDSSecurityGroup(applicationSecurityGroup, vpc) {
         description: "Security group for RDS instances",
         vpcId: vpc.id
     });
-
-    // Add ingress rules for RDS access
-
 
     // Example rule for MySQL on port 3306
     new aws.ec2.SecurityGroupRule("db-ingress-rule-mysql", {
--- rds.js
+++ rds.js
@@ -1,43 +1,32 @@
-/******************************** */
-/*
-// Example usage:
-const vpcId = '<Your-VPC-ID>'; // replace with your VPC ID
-const subnetIdArray = ['<Your-Subnet-ID-1>', '<Your-Subnet-ID-2>']; // replace with your subnet IDs
-
-const { rdsAddress } = createRdsInstance(vpcId, subnetIdArray, dbIdentifier, dbName, dbUsername, dbPassword);
-
-// Export the RDS instance address
-exports.rdsAddress = rdsAddress;
-
-*/
-
-/***************************** */
-
 const pulumi = require("@pulumi/pulumi");
 const aws = require("@pulumi/aws");
 const index = require('./index');
 
-async function createRdsInstance(vpc, subnetIdArray) {
+async function createRdsInstance(dbSecurityGroup, subnetIdArray) {
 
 
     // Database configuration
     const dbIdentifier = 'csye6225';
     const dbName = 'csye6225';
     const dbUsername = 'csye6225';
-    const dbPassword = 'Abhi\$3534';
+    const dbPassword = 'Abhi$3534';
+
+    const dbSubnetGroup = new aws.rds.SubnetGroup("db_subnet_group", {
+        subnetIds: subnetIdArray,
+    });
 
     const rdsInstance = new aws.rds.Instance(dbIdentifier, {
         // Database configuration
         allocatedStorage: 20,
         instanceClass: 'db.t2.micro',
-        name: dbName,
+        dbName: dbName,
         username: dbUsername,
         password: dbPassword,
 
         // General configuration
         publiclyAccessible: false,
-        vpcSecurityGroupIds: [vpc.id],
-        dbSubnetGroupName: subnetIdArray.join(','),
+        vpcSecurityGroupIds: [dbSecurityGroup.id],
+        dbSubnetGroupName: dbSubnetGroup.name,
 
         // Engine configuration
         engine: 'mysql',
@@ -49,7 +38,7 @@ async function createRdsInstance(vpc, subnetIdArray) {
     });
 
     return {
-        rdsAddress: rdsInstance.address
+        rdsInstance: rdsInstance
     };
 }
 
