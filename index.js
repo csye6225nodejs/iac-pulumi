65a893cdb7e026e8edad6da7405db8b7e6bc3853
abhishekvenkata1997 <abhishekvenkata.199715@gmail.com>
2023-11-08 08:57:34 -0500

    add changes to assignment

--- index.js
+++ index.js
@@ -27,6 +27,9 @@ const parameter_name = new pulumi.Config("iac-pulumi").require("parameter_name")
 const max_conn = new pulumi.Config("iac-pulumi").require("max_conn");
 const fromPort = new pulumi.Config("iac-pulumi").require("fromPort");
 const toPort = new pulumi.Config("iac-pulumi").require("toPort");
+const hostedZone = new pulumi.Config("iac-pulumi").require("ZoneId");
+const domainname = new pulumi.Config("iac-pulumi").require("domainname");
+const publicIp = new pulumi.Config("iac-pulumi").require("publicIp");
 
 async function main() {
 
@@ -116,10 +119,21 @@ async function main() {
        // #region User Data Script
        const userDataScript = pulumi.all([rdsinstance.dbName, rdsinstance.username, rdsinstance.password,rdsinstance.address]).apply(([dbname, dbusername, dbpassword, dbhost]) => {
         return `#!/bin/bash
+        sudo chown csye6625user:csye6225group -R /opt/webapp
         echo "DB_NAME=${dbname}" >> /opt/webapp/.env
         echo "DB_USER=${dbusername}" >> /opt/webapp/.env
         echo "DB_PASSWORD=${dbpassword}" >> /opt/webapp/.env
         echo "DB_HOST=${dbhost}" >> /opt/webapp/.env
+        sudo cd /opt/webapp
+        sudo mkdir logs
+        sudo cd logs
+        sudo touch csye6225.log
+        sudo chown csye6225user:csye6225group -R /opt/webapp/logs
+        sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+        -a fetch-config \
+        -m ec2 \
+        -c file:/opt/webapp/config/cloudwatch-config.json \
+        -s
         sudo systemctl enable webapp.service
         sudo systemctl start webapp.service
         `;
@@ -136,6 +150,34 @@ async function main() {
     },{dependsOn: [rdsinstance, applicationSecurityGroup] });
     
 
+    // Creating a new IAM Role
+    const role = new aws.iam.Role("role", {
+        assumeRolePolicy: JSON.stringify({
+            Version: "2012-10-17",
+            Statement: [
+                {
+                    Action: "sts:AssumeRole",
+                    Principal: {
+                        Service: "ec2.amazonaws.com"
+                    },
+                    Effect: "Allow",
+                },
+            ],
+        }),
+    });
+
+    // Creating a new IAM Role Policy Attachment for CloudWatchAgentServerPolicy
+    const cloudWatchFullAccess = new aws.iam.RolePolicyAttachment("cloudWatchAgentServerPolicyAttachment", {
+        role: role.name,
+        policyArn: "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"   // CloudWatchAgentServerPolicy ARN
+    }, {dependsOn: [role]});
+
+    // Creating an IAM Instance Profile which will be later be attached to the EC2 instance
+    const instanceProfile = new aws.iam.InstanceProfile("instanceProfile", {
+        role: role.name
+    },{dependsOn: [role]});
+
+    const logGroup = new aws.cloudwatch.LogGroup("csye6225",{dependsOn:[role]});
         // Create EC2 instance
     const ec2Instance = new aws.ec2.Instance("my-ec2-instance", {
             ami: ami_id, // Replace with your custom AMI ID
@@ -149,10 +191,25 @@ async function main() {
                 volumeType: volumeType, // General Purpose SSD (GP2)
             },
             userData: pulumi.interpolate`${userDataScript}`,
+            role: role.name,
+            iamInstanceProfile: instanceProfile,
             tags: {
                 Name: "Abhishek-EC2Instance", // Replace with a suitable name
             },
-    }, {dependsOn: [rdsinstance,dbSecurityGroup]});
+    }, {dependsOn: [rdsinstance,dbSecurityGroup, role, instanceProfile, cloudWatchFullAccess ]});
+
+    // Get an existing Hosted Zone using its Zone ID
+    let myZone = aws.route53.getZone({ zoneId: hostedZone });   
+
+    // Create or update an A record to point to the public IP addres of the EC2 instance
+    let myRecord = new aws.route53.Record("myRecord", {
+        // Your domain name goes here
+        name: domainname,
+        type: "A",
+        zoneId: hostedZone,
+        records: [ec2Instance.publicIp],
+        ttl: 60,
+    },{dependsOn: [ec2Instance]});
 
     const publicRouteTable = createPublicRouteTable(vpc, publicSubnets, internetGateway);
     const privateRouteTable = createPrivateRouteTable(vpc, privateSubnets);
